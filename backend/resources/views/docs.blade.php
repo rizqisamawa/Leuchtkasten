6280e724b7d3613538b31a64840add00e87213f2
rizqisamawa <rizqisamawa11@gmail.com>
2023-05-05 09:59:30 +0700

    style: docs backend

---
+++ backend/resources/views/docs.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Documentation Leuchtkasten - Backend</title>
+</head>
+
+<body class="bg-slate-800 text-white">
+    <div class="mt-2 flex flex-col items-center justify-center py-2">
+        <div class="text-center font-semibold">
+            <div class="text-4xl uppercase">Documentation</div>
+            <div class="text-xl font-thin">Leuchtkasten - Backend</div>
+        </div>
+
+        <div class="mt-8 h-auto w-[30cm] rounded border border-white bg-gray-100 p-2 p-6 text-black">
+            <div class="">
+                <div class="Main title text-xl font-semibold">Main Title</div>
+                <a href="" class="Main title text-blue-500 hover:text-blue-950">Sub title</a>
+                <p class="mt-2 text-gray-800">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia laborum doloremque, pariatur illo expedita dignissimos, modi assumenda iusto aliquam tenetur veritatis minus vero eos cumque?
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
